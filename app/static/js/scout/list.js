13b5ee8d762b44e0fc822c19d1d2826dfd945737
Jerry <zeruiz@nycstudents.net>
2026-02-05 21:47:13 -0500

    Fixes #32

--- app/static/js/scout/list.js
+++ app/static/js/scout/list.js
@@ -133,11 +133,11 @@ function exportToCSV() {
         const autoFuel = row.querySelector('td:nth-child(4)').textContent.trim();
         const autoClimb = row.querySelector('td:nth-child(5)').textContent.trim();
         const transitionFuel = row.querySelector('td:nth-child(6)').textContent.trim();
-        const teleopFuel = row.querySelector('td:nth-child(7)').textContent.trim();
-        const endgameFuel = row.querySelector('td:nth-child(8)').textContent.trim();
-        const climb = row.querySelector('td:nth-child(9)').textContent.trim();
-        const defense = row.querySelector('td:nth-child(11)').textContent.trim();
-        const robotDisabled = row.querySelector('td:nth-child(12) span').textContent.trim();
+        const teleopFuel = row.querySelector('td:nth-child(8)').textContent.trim();
+        const endgameFuel = row.querySelector('td:nth-child(9)').textContent.trim();
+        const climb = row.querySelector('td:nth-child(10)').textContent.trim();
+        const defense = row.querySelector('td:nth-child(12)').textContent.trim();
+        const robotDisabled = row.querySelector('td:nth-child(13) span').textContent.trim();
         const notes = (row.dataset.notes || '').replace(/,/g, ';').replace(/\n/g, ' ');
         const {scouter} = row.dataset;
         const {eventCode} = row.closest('.event-section').dataset;
